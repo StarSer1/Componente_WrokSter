e79570b8b3c7c579e0a6d19507f68a7e49d11292
wrok <brandonwrok@gmail.com>
2024-03-21 20:32:53 -0700

    Presentacion

--- WrokSter/Form1.Designer.cs
+++ WrokSter/Form1.Designer.cs
@@ -28,81 +28,217 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
             this.radialgauge1 = new Radialgauge.Radialgauge();
             this.radialgauge2 = new Radialgauge.Radialgauge();
             this.radialgauge3 = new Radialgauge.Radialgauge();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(42)))), ((int)(((byte)(98)))));
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(714, 58);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(42)))), ((int)(((byte)(98)))));
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(714, 58);
+            this.panel2.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(42)))), ((int)(((byte)(98)))));
+            this.label5.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(240, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(220, 39);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "RadialGauge";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(42)))), ((int)(((byte)(98)))));
+            this.label4.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(240, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(220, 39);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "RadialGauge";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(42)))), ((int)(((byte)(98)))));
+            this.label1.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(196)))), ((int)(((byte)(244)))));
+            this.label1.Location = new System.Drawing.Point(286, 254);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 39);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Rustico";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(42)))), ((int)(((byte)(98)))));
+            this.label2.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.MediumOrchid;
+            this.label2.Location = new System.Drawing.Point(511, 254);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(159, 39);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Moderno";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(42)))), ((int)(((byte)(98)))));
+            this.label3.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.LightSeaGreen;
+            this.label3.Location = new System.Drawing.Point(46, 254);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 39);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Clasico";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(42)))), ((int)(((byte)(98)))));
+            this.panel3.Controls.Add(this.label6);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel3.Location = new System.Drawing.Point(0, 328);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(714, 32);
+            this.panel3.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(2, 6);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(170, 21);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Libreria: WrokSter";
             // 
             // radialgauge1
             // 
-            this.radialgauge1.AnchoDelPerimetro = 30;
-            this.radialgauge1.ColorDeLineaCentral = System.Drawing.Color.LightSeaGreen;
-            this.radialgauge1.ColorDelPerimetro = System.Drawing.Color.Teal;
-            this.radialgauge1.ColorDelPuntoCentral = System.Drawing.Color.DarkSlateGray;
-            this.radialgauge1.Estilo = Radialgauge.RadialGaugeStyle.Clasico;
-            this.radialgauge1.EstiloDeLineaCentral = Radialgauge.Radialgauge.CentralLineStyle.Triangular;
-            this.radialgauge1.EstiloDelPerimetro = System.Drawing.Drawing2D.DashStyle.Custom;
-            this.radialgauge1.GrosorDeLineaCentral = 6;
-            this.radialgauge1.IntervaloAnimacion = 25;
-            this.radialgauge1.Location = new System.Drawing.Point(28, 12);
+            this.radialgauge1.AnchoDelPerimetro = 5;
+            this.radialgauge1.ColorDeFondo = System.Drawing.Color.Black;
+            this.radialgauge1.ColorDeLineaCentral = System.Drawing.Color.MediumOrchid;
+            this.radialgauge1.ColorDelPerimetro = System.Drawing.Color.RoyalBlue;
+            this.radialgauge1.ColorDelPuntoCentral = System.Drawing.Color.MediumSlateBlue;
+            this.radialgauge1.GrosorDeLineaCentral = 5;
+            this.radialgauge1.Location = new System.Drawing.Point(482, 89);
             this.radialgauge1.Name = "radialgauge1";
-            this.radialgauge1.Size = new System.Drawing.Size(220, 220);
-            this.radialgauge1.TabIndex = 0;
+            this.radialgauge1.PuntoCentral = 4;
+            this.radialgauge1.Size = new System.Drawing.Size(242, 242);
+            this.radialgauge1.TabIndex = 8;
             this.radialgauge1.Text = "radialgauge1";
             this.radialgauge1.Value = 100;
             // 
             // radialgauge2
             // 
-            this.radialgauge2.AnchoDelPerimetro = 5;
-            this.radialgauge2.ColorDeFondo = System.Drawing.Color.Black;
-            this.radialgauge2.ColorDeLineaCentral = System.Drawing.Color.MediumOrchid;
-            this.radialgauge2.ColorDelPerimetro = System.Drawing.Color.RoyalBlue;
-            this.radialgauge2.ColorDelPuntoCentral = System.Drawing.Color.MediumSlateBlue;
+            this.radialgauge2.AnchoDelPerimetro = 30;
+            this.radialgauge2.ColorDeFondo = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(16)))), ((int)(((byte)(43)))));
+            this.radialgauge2.ColorDeLineaCentral = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(196)))), ((int)(((byte)(244)))));
+            this.radialgauge2.ColorDelPerimetro = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(141)))), ((int)(((byte)(184)))));
+            this.radialgauge2.ColorDelPuntoCentral = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.radialgauge2.Estilo = Radialgauge.RadialGaugeStyle.Rustico;
+            this.radialgauge2.EstiloDeLineaCentral = Radialgauge.Radialgauge.CentralLineStyle.Triangular;
+            this.radialgauge2.EstiloDelPerimetro = System.Drawing.Drawing2D.DashStyle.Dash;
             this.radialgauge2.GrosorDeLineaCentral = 5;
-            this.radialgauge2.Location = new System.Drawing.Point(433, 12);
+            this.radialgauge2.Location = new System.Drawing.Point(227, 86);
             this.radialgauge2.Name = "radialgauge2";
-            this.radialgauge2.PuntoCentral = 4;
-            this.radialgauge2.Size = new System.Drawing.Size(242, 242);
-            this.radialgauge2.TabIndex = 1;
+            this.radialgauge2.PuntoCentral = 10;
+            this.radialgauge2.Size = new System.Drawing.Size(262, 262);
+            this.radialgauge2.TabIndex = 9;
             this.radialgauge2.Text = "radialgauge2";
+            this.radialgauge2.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(16)))), ((int)(((byte)(43)))));
             this.radialgauge2.Value = 100;
             // 
             // radialgauge3
             // 
             this.radialgauge3.AnchoDelPerimetro = 30;
-            this.radialgauge3.ColorDeFondo = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(16)))), ((int)(((byte)(43)))));
-            this.radialgauge3.ColorDeLineaCentral = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(196)))), ((int)(((byte)(244)))));
-            this.radialgauge3.ColorDelPerimetro = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(141)))), ((int)(((byte)(184)))));
-            this.radialgauge3.ColorDelPuntoCentral = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
-            this.radialgauge3.Estilo = Radialgauge.RadialGaugeStyle.Rustico;
+            this.radialgauge3.ColorDeLineaCentral = System.Drawing.Color.LightSeaGreen;
+            this.radialgauge3.ColorDelPerimetro = System.Drawing.Color.Teal;
+            this.radialgauge3.ColorDelPuntoCentral = System.Drawing.Color.DarkSlateGray;
+            this.radialgauge3.Estilo = Radialgauge.RadialGaugeStyle.Clasico;
             this.radialgauge3.EstiloDeLineaCentral = Radialgauge.Radialgauge.CentralLineStyle.Triangular;
-            this.radialgauge3.EstiloDelPerimetro = System.Drawing.Drawing2D.DashStyle.Dash;
-            this.radialgauge3.GrosorDeLineaCentral = 5;
-            this.radialgauge3.Location = new System.Drawing.Point(229, 238);
+            this.radialgauge3.EstiloDelPerimetro = System.Drawing.Drawing2D.DashStyle.Custom;
+            this.radialgauge3.GrosorDeLineaCentral = 6;
+            this.radialgauge3.Location = new System.Drawing.Point(12, 105);
             this.radialgauge3.Name = "radialgauge3";
-            this.radialgauge3.PuntoCentral = 10;
-            this.radialgauge3.Size = new System.Drawing.Size(247, 247);
-            this.radialgauge3.TabIndex = 2;
+            this.radialgauge3.Size = new System.Drawing.Size(207, 207);
+            this.radialgauge3.TabIndex = 10;
             this.radialgauge3.Text = "radialgauge3";
-            this.radialgauge3.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(16)))), ((int)(((byte)(43)))));
             this.radialgauge3.Value = 100;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(714, 408);
-            this.Controls.Add(this.radialgauge3);
+            this.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.ClientSize = new System.Drawing.Size(714, 360);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.radialgauge2);
             this.Controls.Add(this.radialgauge1);
+            this.Controls.Add(this.radialgauge3);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label6;
         private Radialgauge.Radialgauge radialgauge1;
         private Radialgauge.Radialgauge radialgauge2;
         private Radialgauge.Radialgauge radialgauge3;
